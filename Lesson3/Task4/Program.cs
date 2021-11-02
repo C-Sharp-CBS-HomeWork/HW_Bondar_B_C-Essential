54dd248bd88ad49489a94dd9f1896f09ad3b896d
Bohdan Bondar <bv.bondar777@gmail.com>
2021-11-02 13:13:07 +0200

    Внес правки в таск4 темы 3

--- Lesson3/Task4/Program.cs
+++ Lesson3/Task4/Program.cs
@@ -8,36 +8,31 @@ namespace Task4
         {
             Console.WriteLine("Добро пожаловать! Введите ваш ключ:");
             string key = Console.ReadLine();
+            DocumentWorker test = null;
             switch (key)
             {
                 case "pro1":
                     {
                         Console.WriteLine("Поздравляю! Вам доступна версия Про!");
-                        DocumentWorker test = new ProDocumentWorker();
-                        test.OpenDocument();
-                        test.EditDocument();
-                        test.SaveDocument();
+                        test = new ProDocumentWorker();
                         break;
                     }
                 case "expert2":
                     {
                         Console.WriteLine("Поздравляю! Вам доступна версия Эксперт!");
-                        DocumentWorker test = new ExpertDocumentWorker();
-                        test.OpenDocument();
-                        test.EditDocument();
-                        test.SaveDocument();
+                        test = new ExpertDocumentWorker();
                         break;
                     }
                 default:
                     {
                         Console.WriteLine("Вам доступна версия базовая версия программы.");
-                        DocumentWorker test = new DocumentWorker();
-                        test.OpenDocument();
-                        test.EditDocument();
-                        test.SaveDocument();
+                        test = new DocumentWorker();
                         break;
                     }
             }
+            test.OpenDocument();
+            test.EditDocument();
+            test.SaveDocument();
             Console.ReadKey();
         }
 
